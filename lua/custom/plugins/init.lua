ede1df40cb93156e29814e66ac776157cf621b28
iywey <62179341+iywey@users.noreply.github.com>
2025-06-26 16:20:30 +0200

    remove comment

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -39,15 +39,6 @@ return {
   --     -- end)
   --   end,
   -- },
-  -- -- https://github.com/ggandor/leap.nvim
-  -- {
-  --   'ggandor/leap.nvim',
-  --   config = function()
-  --     vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-  --     vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-  --     vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
-  --   end,
-  -- },
   {
     'folke/zen-mode.nvim',
     keys = {
